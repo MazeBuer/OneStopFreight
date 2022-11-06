4f78154f021aa935c8c7cfe02ec09f2b70bfc504
MazeBuer <mazikeenbuer@gmail.com>
2022-11-05 21:10:16 -0700

    updated UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,32 @@
 //HAMBURGER MENU
 // For toggling and finding number of children and other stuff is done here!
 
-const navigation = document.getElementById("nav");
-const menu = document.getElementById("menu");
+// const navigation = document.getElementById("nav");
+// const menu = document.getElementById("menu");
 
-menu.addEventListener("click", () => {
-  // The navigation.children.length means the following :-
-  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
-  //   Yup That's all.
-  navigation.style.setProperty("--childenNumber", navigation.children.length);
+// menu.addEventListener("click", () => {
+//   // The navigation.children.length means the following :-
+//   // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+//   //   Yup That's all.
+//   navigation.style.setProperty("--childenNumber", navigation.children.length);
 
-  //    Casually Toggling Classes to make them animate on click
-  //   Regular stuff ;)
-  navigation.classList.toggle("active");
-  menu.classList.toggle("active");
+//   //    Casually Toggling Classes to make them animate on click
+//   //   Regular stuff ;)
+//   navigation.classList.toggle("active");
+//   menu.classList.toggle("active");
+// });
+$(document).ready(function() {
+  $(".nav-toggler").each(function(_, navToggler) {
+    var target = $(navToggler).data("target");
+    $(navToggler).on("click", function() {
+      $(target).animate({
+        height: "toggle"
+      });
+    });
+  });
 });
+
+
 
 //SCROLL DOWN FROM MENU
 
